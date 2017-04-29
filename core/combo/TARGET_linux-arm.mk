0c8395af223c9cc5d8687c8d3a254f995c3df39c
xano <denis.kraemer@gmx.net>
2017-04-29 13:24:02 +0200

    use optimized compile options for snapdragon llvm compiler
    Change-Id: Ie3b25661cbf084eb8f41f3d8461b4c76894d7e4b

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -71,7 +71,7 @@ endef
 
 $(combo_2nd_arch_prefix)TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -O2 \
+$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -Ofast \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
                         -funswitch-loops
